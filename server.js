f3c1811100671869d5824abb0f6f195691461bb8
daisy <zacaopingzi@gmail.com>
2020-08-11 22:38:44 +1000

    modify server.js

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.use(function (req, res, next) {
 	res.status(404).send('404')
 });
 
+process.on('uncaughtException', function (err) {
+    console.log(err);
+}); 
+
 app.use(function (err, req, res, next) {
 	console.error(err.stack);
 	res.status(500).send('500');
