116ce646545eba9d41c8257da2b3fecff71e9ed7
daisy <zacaopingzi@gmail.com>
2020-08-11 20:03:54 +1000

    fix loop logic

--- weatherapp/src/app/weather/weather.component.ts
+++ weatherapp/src/app/weather/weather.component.ts
@@ -8,12 +8,12 @@ import { GetWeatherService } from "../get-weather.service";
 })
 export class WeatherComponent implements OnInit {
   cities = ["Sydney", "Melbourne", "Brisbane"];
+  city = "Sydney";
   temperature = null;
   description = null;
   temp_min = null;
   temp_max = null;
   iconUrl = null;
-  city = "Sydney";
   saved_data = [
     {
       city: "Sydney",
@@ -21,7 +21,7 @@ export class WeatherComponent implements OnInit {
       description: null,
       temp_min: null,
       temp_max: null,
-      iconUrl: null
+      iconUrl: null,
     },
     {
       city: "Melbourne",
@@ -29,7 +29,7 @@ export class WeatherComponent implements OnInit {
       description: null,
       temp_min: null,
       temp_max: null,
-      iconUrl: null
+      iconUrl: null,
     },
     {
       city: "Brisbane",
@@ -37,7 +37,7 @@ export class WeatherComponent implements OnInit {
       description: null,
       temp_min: null,
       temp_max: null,
-      iconUrl: null
+      iconUrl: null,
     },
   ];
 
@@ -45,12 +45,13 @@ export class WeatherComponent implements OnInit {
 
   ngOnInit() {}
 
-  getCity(city) {
-    this.city = city;
-    // make a loop
+  // initial city is Sydney; call getCity when click the button
+  getCity() {
     const pre_city = this.cities.splice(0, 1)[0];
+    // add the pre_city to the array
     this.cities.splice(2, 0, pre_city);
     const current_city = this.cities[0];
+    this.city = current_city;
     // save the data
     this.saved_data.forEach((item) => {
       if (item.city == current_city) {
