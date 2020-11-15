d726a31d8e71f0ea130a6c92e999dd41b0dbe738
daisy <zacaopingzi@gmail.com>
2020-11-15 20:15:53 +1100

    added database

--- function.js
+++ function.js
@@ -2,12 +2,13 @@ const axios = require("axios");
 
 module.exports.weather = async function (query) {
   const url = process.env.REST_ENDPOINT + query.city + ",au&units=metric&appid=" + process.env.KEY;
-  result = await axios.get(url);
+  const result = await axios.get(url);
   const description = result.data.weather[0].description;
   const icon = result.data.weather[0].icon;
   const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
   // the result contains many thigns only need temp, min temp, max temp, descrip, icon
   const neededData = {
+    city: result.data.name,
     temp: result.data.main.temp,
     temp_min: result.data.main.temp_min,
     temp_max: result.data.main.temp_max,
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
-const cors = require('cors');
+const mongoose = require("mongoose");
+const cors = require("cors");
 const bodyParser = require("body-parser");
+const app = express();
 
 const func = require("./function");
 
@@ -12,22 +13,66 @@ app.use(bodyParser.json());
 
 // Create link to Angular build directory
 app.use(express.static(__dirname + "/public"));
-			
-app.get("/:city", async function(req, res){
-res.json(await func.weather(req.params)); // the city name
-});
 
-app.listen(process.env.PORT || 8080, () => console.log('Server is running!'));
+// connect to database
+mongoose.Promise = global.Promise;
+const connectionOptions = {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
 
+mongoose
+  .connect(process.env.DB_URI, connectionOptions)
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database.", err);
+    process.exit();
+  });
+
+const weatherSchema = mongoose.Schema(
+  {
+    city: String,
+    temperature: Number,
+    description: String,
+    temp_min: Number,
+    temp_max: Number,
+    iconUrl: String,
+  },
+  { timestamps: true }
+);
+const Weather = mongoose.model("Weather", weatherSchema);
+
+app.get("/:city", async function (req, res) {
+	let result = await func.weather(req.params);
+	const weather = new Weather({
+	  city: result.city,
+	  temperature: result.temp,
+	  description: result.descrip,
+	  temp_min: result.temp_min,
+	  temp_max: result.temp_max,
+	  iconUrl: result.iconUrl,
+	});
+	// save the weather info in the database
+	weather.save();
+	res.json(result);
+  });
+
+// error handle
 app.use(function (req, res, next) {
-	res.status(404).send('404')
+  res.status(404).send("404");
 });
 
-process.on('uncaughtException', function (err) {
-    console.log(err);
-}); 
+process.on("uncaughtException", function (err) {
+  console.log(err);
+});
 
 app.use(function (err, req, res, next) {
-	console.error(err.stack);
-	res.status(500).send('500');
+  console.error(err.stack);
+  res.status(500).send("500");
 });
+
+app.listen(process.env.PORT || 8080, () => console.log("Server is running!"));
